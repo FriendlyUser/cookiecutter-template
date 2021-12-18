e9d8e28e1ce1a415cae3df1b4b1e195b6d0b58c4
David Li <davidli012345@gmail.com>
2021-12-18 22:13:42 +0000

    fix: fix install

---
+++ {{cookiecutter.project_slug}}/main.py
@@ -0,0 +1,8 @@
+from icecream import ic
+
+def main():
+    ic("Eating ice cream")
+    pass
+
+if __name__ == "__main__":
+    main()
--- {{cookiecutter.project_slug}}/setup.sh
+++ {{cookiecutter.project_slug}}/setup.sh
@@ -1,8 +1,10 @@
-mkdir -p ~/.streamlit/
+# mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# " > ~/.streamlit/config.toml
+
+python3 -m pip install -r requirements.txt
